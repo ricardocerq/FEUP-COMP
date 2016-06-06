a54d8b1fef5b7cca605791b8c23cef35aefd6e2c
444Duarte <duartemr.pinto@gmail.com>
2016-06-06 17:28:34 +0100

    Infinite cicle. bug

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ import stmtparser.StmtParser;
 public class Test {
 	
 	public static void main(String args[]){
-		StmtParser parser = new StmtParser("test.txt");
+		StmtParser parser = new StmtParser("test/test.txt");
 		parser.run();
 	}
 }
